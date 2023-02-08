3076b9339aa152e0c0f5f429149ce08ab812ca7b
vasilie <vasilie.91@gmail.com>
2023-02-08 08:14:10 +0100

    Add initial scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Plane, Grid, Stage, Environment, AccumulativeShadows, RandomizedLight, } from '@react-three/drei'
+import './index.css';
+import Box from "./components/Box";
+import { generateCubes } from './lib/helpers/sceneGeneration';
+// const radians = angleInDegrees * Math.PI / 180
+//import { CompressedPixelFormat } from 'three';
 
-function App() {
+export default function App() {
+
+  const cubes = generateCubes(16,7);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Canvas shadows camera={{ position: [-15, 7, 10]}}>
+        <Grid renderOrder={-1} sectionColor={[0.21, 0.21, 0.21, 0.2]} opacity={0.1} position={[0, 0.001, 0]} infiniteGrid/>
+        <ambientLight intensity={0.3}  />
+        <directionalLight position={[10, 10, 10]} angle={0.3} penumbra={0.3} castShadow intensity={0.3}/>
+        <Stage intensity={0.1} environment="city" shadows="contact" adjustCamera={false} />
+        <Box position={[0, 0.5, 0]} material-color="#7AF5E2" />
+        <Plane castShadow receiveShadow args={[50, 50]} rotation={[-Math.PI / 2, 0, 0]} />
+        <Environment background preset="sunset" blur={0.8} castShadow/>
+        <OrbitControls autoRotate autoRotateSpeed={0.05} enableZoom={false} makeDefault polarAngle={3 * Math.PI /13} minPolarAngle={Math.PI/ 6} maxPolarAngle={Math.PI / 2.01}  />
+        {cubes.map(cube => <Box color="#F5C001" {...cube} />)}
+      </Canvas>
     </div>
-  );
+  )
 }
-
-export default App;
