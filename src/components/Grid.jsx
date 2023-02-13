5beb24e4af578c3d01e2f5ab658b3598849293ff
vasilie <vasilie.91@gmail.com>
2023-02-13 14:11:21 +0100

    Update grid generation with snap angle

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,9 +4,9 @@ import { getAngleFromLengthAndRadius, getPointOnACircle, toRadians  } from "../l
 import { generateCurvedLinePoints  } from "../lib/helpers/sceneGeneration";
 import Cell from "./Cell";
 
-const Grid = ({gridSizeX, gridSizeY, radius}) => {
+const Grid = ({gridSizeX, gridSizeY, radius, snapAngle}) => {
 
-  const angle = -Math.abs(getAngleFromLengthAndRadius(gridSizeX, radius));
+  const angle = -Math.abs(getAngleFromLengthAndRadius(gridSizeX, radius, snapAngle));
   const radiusLength = 2 * Math.PI * radius;
   const maxCells = Math.floor(radiusLength / gridSizeX);
   const circlePoints = generateCurvedLinePoints(50, radius, 360 ,0.31);
@@ -16,22 +16,22 @@ const Grid = ({gridSizeX, gridSizeY, radius}) => {
   });
 
   const { debugGrid } = useControls({
-    debugGrid: { value: true },
+    debugGrid: { value: false },
   });
 
   const generateRow = (gridSizeX, radius) => {
     const radiusLength = 2 * Math.PI * radius;
-    const maxCells = Math.floor(radiusLength / gridSizeX);
-    const angle = getAngleFromLengthAndRadius(gridSizeX, radius) 
+    const maxCells = Math.floor(radiusLength / gridSizeX); 
+    const angle = getAngleFromLengthAndRadius(gridSizeX, radius, snapAngle);
     const row = [];
     for (let i = 0; i < maxCells; i++) {
-      const position = getPointOnACircle(i * angle, radius)
+      const position = getPointOnACircle(i * angle, radius);
       row.push(position);
     }
     return row;
   }
 
-  const AngleLines = () => {
+  const DebugAngleLines = () => {
     let lines = [];
     for (let i = 0; i < maxCells; i++) {
       lines.push(<Line color="blue" points={[[0,0.01,0], getPointOnACircle(toRadians(angle * i), 3 * radius, 0.1)]} />);
@@ -39,9 +39,9 @@ const Grid = ({gridSizeX, gridSizeY, radius}) => {
     return lines;
   }
 
-  const RowLines = () => {
+  const DebugRowLines = () => {
     let lines = [];
-    for (let i = 0; i < maxRows; i++) {
+    for (let i = 0; i < maxRows + 1; i++) {
       lines[i] = <Line color="green" points={generateCurvedLinePoints(50, radius + gridSizeY * i, 360 ,0.1)}/>  ;
     }
     return lines;
@@ -59,7 +59,7 @@ const Grid = ({gridSizeX, gridSizeY, radius}) => {
           radius={radius + gridSizeY * i}
           gridSizeX={gridSizeX}
           gridSizeY={gridSizeY}
-          angle={-getAngleFromLengthAndRadius(gridSizeX, radius + gridSizeY * i)  * Math.PI / 180}
+          angle={-getAngleFromLengthAndRadius(gridSizeX, radius + gridSizeY * i, snapAngle)  * Math.PI / 180}
           key={`cell${j}`}
         ></Cell>
         )
@@ -72,14 +72,15 @@ const Grid = ({gridSizeX, gridSizeY, radius}) => {
   return (
     <>
      <Rows />
-      {debugGrid && (
+    {debugGrid && (
       <>
-        <Line color="red" points={[[0,0.01,0], [0,0.01,40]]} />
-        <RowLines />
-        <AngleLines />
-        <Line color="green" points={circlePoints}/>
+        <Line color="red" points={[[0,0.01,0], [0,0.01,40]]} lineWidth={2}/>
+        <DebugRowLines />
+        <DebugAngleLines />
+        <Line color="orange" points={circlePoints}/>
+        <Line color="green" points={[[0,0.01,0], getPointOnACircle(-toRadians(snapAngle), radius + 25, 0.01)]} lineWidth={2}  />
       </>
-      )}
+    )}
     </>
   );
 }
