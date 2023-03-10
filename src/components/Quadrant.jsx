472558cf62193edf6bebebc1db41dc0c9a728906
vasilie <vasilie.91@gmail.com>
2023-03-10 23:29:51 +0100

    Create quadrants

---
+++ src/components/Quadrant.jsx
@@ -0,0 +1,37 @@
+import { toRadians } from "../lib/helpers/math";
+import { Line } from "@react-three/drei";
+import { getPointOnACircle } from "../lib/helpers/math";
+import { useContext } from "react";
+import { GridContext } from "../Contexts/GridContext";
+import Pavement from "./Pavement";
+
+const Quadrant = ({startingAngle, pullbackPosition }) => {
+  const { quadrantAngle, radius, maxRows, gridSizeY, debugGrid } = useContext(GridContext);
+  const pavements = [];
+
+  for (let i = 0; i < maxRows; i++) {
+    const pavementRadius = radius + gridSizeY * i;
+    pavements[i] = <Pavement startingAngle={startingAngle} radius={pavementRadius}></Pavement>
+  }
+
+  return (
+    <group position={pullbackPosition}>
+      {debugGrid && <>
+        <Line color="purple" points={[[0,0.01,0], getPointOnACircle(toRadians(startingAngle), 3 * radius, 0.1)]} />
+        <Line color="purple" points={[[0,0.01,0], getPointOnACircle(toRadians(startingAngle + quadrantAngle), 3 * radius, 0.1)]} />
+      </>}
+      {pavements}
+    </group>
+  );
+}
+
+
+Quadrant.propTypes = {
+
+};
+
+Quadrant.defaultProps = {
+
+};
+
+export default Quadrant;
---
+++ src/components/Quadrants.jsx
@@ -0,0 +1,53 @@
+import Pavement from "./Pavement";
+import { toRadians } from "../lib/helpers/math";
+import Quadrant from "./Quadrant";
+import { useContext, useState, useEffect } from "react";
+import { GridContext } from "../Contexts/GridContext";
+import { getPointOnACircle } from "../lib/helpers/math";
+
+
+const Quadrants = () => {
+  
+  const { quadrantAngle, radius } = useContext(GridContext);
+  const [quadrants, setQuadrants] = useState([]);
+  
+  const generateQuadrants = () => {
+    const quadrantsLength = 270 / quadrantAngle;
+    let _quadrants = [];
+
+    for (let i = 0; i < quadrantsLength; i++) {
+      const quadrantRowZeroMiddlePosition = getPointOnACircle(toRadians(- 45 * i - 22.5), radius, 0.1);
+      const quadrantMovePosition = getPointOnACircle(toRadians(- 45 * i - 22.5), radius + 1.3, 0.1);
+      const quadrantDistanceToMove = [quadrantMovePosition[0] - quadrantRowZeroMiddlePosition[0], 0, quadrantMovePosition[2] - quadrantRowZeroMiddlePosition[2]];
+      
+      _quadrants.push({
+        index: i,
+        startingAngle: quadrantAngle * i,
+        pullbackPosition:  quadrantDistanceToMove,
+      });
+    }
+   return _quadrants;
+  }
+
+  useEffect(()=>{
+    generateQuadrants();
+    setQuadrants(generateQuadrants());
+  },[]);
+
+  return (
+    <group>
+      {quadrants.map(quadrant => <Quadrant {...quadrant}></Quadrant>)}
+    </group>
+  );
+}
+
+
+Quadrants.propTypes = {
+
+};
+
+Quadrants.defaultProps = {
+
+};
+
+export default Quadrants;
