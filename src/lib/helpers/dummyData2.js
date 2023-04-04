3ddc564a328183c611a6fc07b61f3ec544f84ee4
vasilie <vasilie.91@gmail.com>
2023-04-04 16:37:32 +0200

    Update styles

---
+++ src/lib/helpers/dummyData2.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+      "account_id": "a2f453a6-5d7b-48c1-930e-fbc4cf58a710",
+      "booth_description": "deleniti voluptatem necessitatibus",
+      "company_logo_url": "https://github.githubassets.com/images/modules/site/copilot/copilot.webp",
+      "company_name": "NSZ",
+      "contact_email": null,
+      "description": "Long",
+      "description_short": "Short",
+      "facebook": null,
+      "hire_minorities": null,
+      "hire_returning": null,
+      "id": "a2f453a6-5d7b-48c1-930e-fbc4cf58a710",
+      "inbox_id": null,
+      "instagram": null,
+      "linkedin": null,
+      "logo": null,
+      "object": "account",
+      "sector_code": "B",
+      "sector_name": null,
+      "twitter": null,
+      "website": null,
+      "youtube": null
+  },
+];
